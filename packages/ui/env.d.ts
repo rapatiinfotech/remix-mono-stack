54ae7768262891148f323af9db0d9939193ba7d2
Soheb Rapati <soheb@cakewalk.in>
2025-08-17 18:11:59 +0400

    fix: resolve pre-commit hook failures blocking

---
+++ packages/ui/env.d.ts
@@ -0,0 +1,11 @@
+// Add SVG module declarations
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.svg?react' {
+    import React from 'react';
+    const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    export default ReactComponent;
+}
---
+++ packages/ui/src/components/pages/landingPage.tsx
@@ -0,0 +1,10 @@
+import OnBoardLayout from '../layouts/obBoardLayout';
+import { Welcome } from '../welcome/welcome';
+
+export default function LandingPage() {
+    return (
+        <OnBoardLayout>
+            <Welcome />
+        </OnBoardLayout>
+    );
+}
